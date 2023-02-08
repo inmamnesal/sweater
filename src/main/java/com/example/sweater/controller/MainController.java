5fce8a203139e23045217b2db2a7ddf52aad77b1
Denis C <denischirchir@gmail.com>
2023-02-08 22:46:41 +0300

    lesson 5 done

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -3,9 +3,11 @@ package com.example.sweater.controller;
 import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +19,26 @@ public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
 //    public String greeting(Map<String,Object> model) {
-    public String greeting() {
+    public String greeting(Map<String,Object> model) {
+        model.put("userName", userService.getPrincipalUserName());
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String,Object> model){
+    public String main(@RequestParam (required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.sweater.service;
 
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,4 +19,11 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
     }
+
+    public String getPrincipalUserName() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal instanceof User ? ((User) principal).getUsername() : String.valueOf(principal);
+    }
+
+
 }
